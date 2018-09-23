e48a53e53cee49357c506db3a9def73bac31bdd8
wii3 <42401274+pannila@users.noreply.github.com>
2018-09-23 22:06:23 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,68 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = ","
+
+
+console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+console.log('         [Wait please .. ]       ')
+console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+client.on('ready', () => {
+	console.log('')
+	console.log('')
+	console.log('')
+	console.log('')
+	console.log('')
+	console.log('')
+	console.log('')
+	console.log('')
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`Logged in as [ ${client.user.tag}! ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log('[           BOT IS ONLINE         ]')
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log('[        info         ]')
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+});
+ 
+client.on("message", message => {
+ 
+  function discoRole() {
+    let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    roles.forEach((role) => {
+      let theRole = message.guild.roles.find("name", role);
+      theRole.edit({color: random}).catch(e => {
+        return message.channel.send(":x: **Error:** The role you specified in the `config.json` is either not a role on this server, or his a role higher than the highest role that I have.");
+      });
+    });
+  }
+ 
+  if(message.content.startsWith(prefix + "herostart")) {
+    if(allowedUsers.includes(message.author.id)) {
+    setInterval(() => { discoRole(); }, config.ms);
+    message.channel.send("```css\nYou now have color roll...```");
+  } else {
+    message.reply(`You do not have permission to Hero. Continue with the bot owner @youssef_tube#5800`);
+  }
+} else
+ 
+if(message.content.startsWith(prefix + "herostop")) {
+  if(allowedUsers.includes(message.author.id)) {
+  message.channel.send("I've stopped discoing.");
+  setTimeout(() => { console.log(process.exit(0)); }, 300);
+} else {
+  message.reply(`You do not have permission to Hero. Continue with the bot owner @youssef_tube#5800`);
+  }
+}
+ 
+});
+const config = require('./config.json');
+const allowedUsers = config.allowedUsers;
+const roles = config.roleToDisco;
+
+client.login(process.env.BOT_TOKEN);
