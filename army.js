4e798a76c7f746508203a5fd6b3a5db459b837d9
wii3 <42401274+pannila@users.noreply.github.com>
2018-10-02 00:08:45 +0200

    Update army.js

--- army.js
+++ army.js
@@ -41,27 +41,6 @@ if(!args[0]) return message.reply('مرجو كتابة نص الدي تريد');
            })
 }
 });
-
- client.on('message', message => {
-	 var prefix = ",";
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-  
- 
-
-if (command == "z") {
-    let say = new Discord.RichEmbed()
-        .setTitle('Text emboss :')
-   message.channel.send(`\n ${zalgo(args.join(' '))}`);
-  }
-
-});
-
 client.on("message", message => {
  if (message.content === ",help") {
   const embed = new Discord.RichEmbed()  
